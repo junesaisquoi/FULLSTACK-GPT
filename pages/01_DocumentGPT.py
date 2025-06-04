88679d15f52884cb5aa1e9a5d0f621994b9c8b4b
junesaisquoi <hojunpark90@gmail.com>
2025-06-04 22:29:36 +0200

    #7.4 Chat Messages - Conversations

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -3,19 +3,26 @@ import streamlit as st
 
 st.title("DocumentGPT")
 
-with st.chat_message("human"):
-    st.write("Hello")
-    
-with st.chat_message("ai"):
-    st.write("Hi, how are you?")
-    
-with st.status("Embedding file...", expanded=True) as status:
-    time.sleep(2)
-    st.write("Fetching the file")
-    time.sleep(2)
-    st.write("Embedding the file")
-    time.sleep(2)
-    st.write("Caching the file")
-    status.update(label="Error", state="error")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
-st.chat_input("Send a message to AI")
+  
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state["messages"].append({"message":message, "role":role})
+        
+for message in st.session_state["messages"]:
+    send_message(message["message"],message["role"], save=False)
+    
+
+message =st.chat_input("Send a message to AI")
+
+if message:
+    send_message(message, "human")
+    time.sleep(2)
+    send_message(f"You sad: {message}", "ai")
+    
+    with st.sidebar:
+        st.write(st.session_state)
