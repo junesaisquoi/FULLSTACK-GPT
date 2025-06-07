8fd4268cff2c09655d4fb721cf154df2ffd0b0fc
junesaisquoi <hojunpark90@gmail.com>
2025-06-07 22:40:06 +0200

    #9.4 Formatter Prompt

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -22,6 +22,158 @@ llm = ChatOpenAI(
     callbacks=[StreamingStdOutCallbackHandler()],
 )
 
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+questions_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", 
+             """
+             You are a helpful assistant that is role-playing as a teacher. 
+             Based ONLY on the following context, make 10 questions to test the user's knowledge about the text.
+             Each question should have 4 options, three of which must be incorrect and one should be correct.
+             Use (o) to signify the correct answer.
+             
+             Qeustion examples:
+             Question 1: What is the color of the ocean?
+             Answer: Red|Yellow|Blue (o)|Green
+             
+             Question 2: What is the capital of Georgia?
+             Answer: Baku|Tbilisi (o)|Manila|Beirut
+             
+             Question 3: When was Avatar released?
+             Answer: 2009 (o)|2010|2011|2008
+             
+             Question: Who was Julius Caesar?
+             Answer: A Roman general and statesman (o)|A Greek philosopher|A Chinese emperor|An Egyptian pharaoh
+             
+             Your turn!
+             
+             Context: {context}
+             """),
+        ]
+    )
+
+questions_chain = {"context": format_docs} | questions_prompt | llm
+
+formatting_prompt = ChatPromptTemplate.from_messages([
+    ("system", 
+     """
+     You are a powerful formatting algorithm that formats quiz questions and answers.
+     You format exam questions into a JSON format that can be used in a quiz application.
+     The options with (o) signify the correct answer.
+     Example Input:
+      Question: What is the color of the ocean?
+        Answers: Red|Yellow|Green|Blue(o)
+         
+        Question: What is the capital or Georgia?
+        Answers: Baku|Tbilisi(o)|Manila|Beirut
+            
+        Question: When was Avatar released?
+        Answers: 2007|2001|2009(o)|1998
+            
+        Question: Who was Julius Caesar?
+        Answers: A Roman Emperor(o)|Painter|Actor|Model
+        
+        Example Output:
+        ```json
+        {{ "questions": [
+                {{
+                    "question": "What is the color of the ocean?",
+                    "answers": [
+                            {{
+                                "answer": "Red",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Yellow",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Green",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Blue",
+                                "correct": true
+                            }},
+                    ]
+                }},
+                            {{
+                    "question": "What is the capital or Georgia?",
+                    "answers": [
+                            {{
+                                "answer": "Baku",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Tbilisi",
+                                "correct": true
+                            }},
+                            {{
+                                "answer": "Manila",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Beirut",
+                                "correct": false
+                            }},
+                    ]
+                }},
+                            {{
+                    "question": "When was Avatar released?",
+                    "answers": [
+                            {{
+                                "answer": "2007",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "2001",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "2009",
+                                "correct": true
+                            }},
+                            {{
+                                "answer": "1998",
+                                "correct": false
+                            }},
+                    ]
+                }},
+                {{
+                    "question": "Who was Julius Caesar?",
+                    "answers": [
+                            {{
+                                "answer": "A Roman Emperor",
+                                "correct": true
+                            }},
+                            {{
+                                "answer": "Painter",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Actor",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Model",
+                                "correct": false
+                            }},
+                    ]
+                }}
+            ]
+        }}
+    ```
+    Your turn!
+
+    Questions: {context}
+
+     """),
+])
+
+formatting_chain = formatting_prompt | llm
+
 # Build (and cache) a retriever from the uploaded file
 @st.cache_resource(show_spinner="Loading file…")
 def split_file(file):
@@ -38,8 +190,7 @@ def split_file(file):
     docs = loader.load_and_split(text_splitter=splitter)
     return docs
 
-def format_docs(docs):
-    return "\n\n".join(document.page_content for document in docs)
+
 
 with st.sidebar:
     docs = None
@@ -72,38 +223,12 @@ if not docs:
         """
     )
 else:
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", 
-             """
-             You are a helpful assistant that is role-playing as a teacher. 
-             Based ONLY on the following context, make 10 questions to test the user's knowledge about the text.
-             Each question should have 4 options, three of which must be incorrect and one should be correct.
-             Use (o) to signify the correct answer.
-             
-             Qeustion examples:
-             Question 1: What is the color of the ocean?
-             Answer: Red|Yellow|Blue (o)|Green
-             
-             Question 2: What is the capital of Georgia?
-             Answer: Baku|Tbilisi (o)|Manila|Beirut
-             
-             Question 3: When was Avatar released?
-             Answer: 2009 (o)|2010|2011|2008
-             
-             Question: Who was Julius Caesar?
-             Answer: A Roman general and statesman (o)|A Greek philosopher|A Chinese emperor|An Egyptian pharaoh
-             
-             Your turn!
-             
-             Context: {context}
-             """),
-        ]
-    )
-    
-    chain = {"context": format_docs} | prompt | llm
-    
     start = st.button("Generate Quiz Questions")
     
     if start:
-        chain.invoke(docs)
+        questions_response = questions_chain.invoke(docs)
+        st.write(questions_response.content)
+        formatting_response = formatting_chain.invoke({
+            "context": questions_response.content
+        })
+        st.write(formatting_response.content)
